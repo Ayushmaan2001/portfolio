e8e0eb37132b87060c53d436e42c4068202d74cc
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2023-06-01 14:55:16 +0530

    res img

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = ({title,d1,d2,d3,tech,site,github,img}) => {
             <Projectdetails title={title} d1={d1} d2={d2} d3={d3} img={tech} site={site} github={github}/>
             </div>
             <div className='flex flex-row jutify-end items-center'>
-            <img src={img} className='w-[18.5em] h-[12rem] mt-[5rem] md:mt-0 md:w-[30rem] md:h-[20rem]' alt='cover'/>
+            <img src={img} className='w-[18.5em] h-[12rem] mt-[2rem] md:mt-0 md:w-[30rem] md:h-[20rem]' alt='cover'/>
             </div>
         </div>
     );
